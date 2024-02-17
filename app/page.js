23da205d068248f30070c08953024bb591fd3c79
Ali Arain <deistali2@gmail.com>
2024-02-17 20:25:50 +0500

    video added

--- app/page.js
+++ app/page.js
@@ -4,7 +4,12 @@ export default function Home() {
   return (
    <div>
     <div className="grid lg:grid-cols-12 mx-auto px-4 py-8 max-w-screen">
+      <div className="lg:col-span-7">
+        <h1>Generate Your Dream Room In One Click</h1>
+      </div>
+      <div className="lg:col-span-5">
 
+      </div>
     </div>
   
 
